69fa4c30f32f0e2ff872c753dba65402cac54873
Ivan Komov <ivankomov97@gmail.com>
2023-04-17 22:04:15 +0700

    Доработан тест проверки информации о контактах на главной странице: теперь информация о контакте с главной страницы сравнивается с информацией о контакте, загруженой из базы данных.

--- test/test_contact_on_home_page.py
+++ test/test_contact_on_home_page.py
@@ -1,18 +1,16 @@
 import re
-from random import randrange
 
 
-def test_contact_on_home_page(app):
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_full_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    print(merge_phones_like_on_home_page(contact_from_edit_page))
+def test_contact_on_home_page(app, db):
+    contacts_from_db = db.get_contact_list()
+    for contact_from_home_page in app.contact.get_contact_list():
+        contact_from_db = contacts_from_db[contacts_from_db.index(contact_from_home_page)]
+        assert contact_from_home_page.firstname == contact_from_db.firstname.strip()
+        assert contact_from_home_page.lastname == contact_from_db.lastname.strip()
+        assert contact_from_home_page.address == contact_from_db.address.strip()
+        assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db)
+        assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+        print(merge_phones_like_on_home_page(contact_from_db))
 
 
 def clear(s):
@@ -25,6 +23,10 @@ def merge_phones_like_on_home_page(contact):
                                                                         contact.secondaryHomePhone])))
 
 
+def delete_extra_spaces(s):
+    return re.sub(" +", " ", s)
+
+
 def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "", [contact.email1, contact.email2,
-                                                                        contact.email3]))
+    return "\n".join(filter(lambda x: x != "", map(lambda x: delete_extra_spaces(x), [contact.email1.strip(), contact.email2.strip(),
+                                                                        contact.email3.strip()])))
